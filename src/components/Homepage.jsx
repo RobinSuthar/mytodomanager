6aa0044285f0663c93c8db1533e75cdc2a6ba012
RobinSuthar <152766032+RobinSuthar@users.noreply.github.com>
2025-01-14 01:39:53 -0700

    Added Hero Section looks good

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -4,7 +4,9 @@ import { useMutation } from "react-query";
 import { IndiviualTodo } from "./IndiviualTodo";
 import { LeftSideNavBar } from "./NavigationBar/LeftSideNavBar";
 import { Main } from "./Hero/Main";
-
+import tickiamge from "./Images/wrong.png";
+import done from "./Images/tick.png";
+import NotDone from "./Images/CircleWithoutHover.png";
 export function HomePage() {
   const [globaltodos, setGlobalTodos] = useState([]);
   const [buttonPressed, SetButtonPressed] = useState(0);
@@ -49,45 +51,89 @@ export function HomePage() {
         </nav>
       </div>
 
-      <div id="RightSideDiv " className="grid grid-rows-3 grid-cols-3 gap-4">
-        <div></div>
-        <div>
+      <div id="RightSideDiv " className="">
+        <div className="flex-col justify-center ml-80">
           <Main></Main>
+          <div className="mt-8  flex flex-row ">
+            <img src={tickiamge} height={6} width={26}></img>
+            <div className="text-sm font-medium mt-1">Over Due</div>
+          </div>
+
+          <div></div>
+          {globaltodos.map((EachElemet) => {
+            console.log(EachElemet);
+            var isCompleted = EachElemet.isCompleted;
+            if (isCompleted) {
+              return (
+                <div key={EachElemet._id} className="mt-4">
+                  <DisplayGlobalTodos
+                    title={EachElemet.title}
+                    description={EachElemet.description}
+                    isCompleted={EachElemet.isCompleted}
+                    owner={EachElemet.type}
+                    time={EachElemet.createdAt}
+                  ></DisplayGlobalTodos>{" "}
+                </div>
+              );
+            } else {
+              return;
+            }
+          })}
+          <div className="mt-8  flex flex-row ">
+            <img src={done} height={6} width={26}></img>
+            <div className="text-sm font-medium mt-1">Completed</div>
+          </div>
+          {globaltodos.map((EachElemet) => {
+            var isCompleted = EachElemet.isCompleted;
+            const time = EachElemet.createdAt;
+            console.log(time);
+            if (!isCompleted) {
+              return (
+                <div key={EachElemet._id} className="mt-4">
+                  <DisplayGlobalTodos
+                    title={EachElemet.title}
+                    description={EachElemet.description}
+                    isCompleted={EachElemet.isCompleted}
+                    owner={EachElemet.type}
+                    time={EachElemet.createdAt}
+                  ></DisplayGlobalTodos>{" "}
+                </div>
+              );
+            } else {
+              return;
+            }
+          })}
         </div>
-        <div></div>
-        {globaltodos.map((EachElemet) => {
-          return (
-            <div key={EachElemet._id}>
-              <DisplayGlobalTodos
-                title={EachElemet.title}
-                description={EachElemet.description}
-                isCompleted={EachElemet.isCompleted}
-              ></DisplayGlobalTodos>{" "}
-            </div>
-          );
-        })}
       </div>
+      <div>asdasd</div>
     </div>
   );
 }
 
 function DisplayGlobalTodos(props) {
-  const IsCompleted = props.isCompleted;
-
-  if (IsCompleted) {
-    return (
-      <div>
-        <h1>Done</h1>
-        <h1 className="text-xl">{props.title}</h1>
-        <h3 className="text-sm">{props.description}</h3>
-      </div>
-    );
-  }
+  const isCompleted = props.isCompleted;
   return (
-    <div>
-      <h1>NOT DONE</h1>
-      <h1 className="text-xl">{props.title}</h1>
-      <h3 className="text-sm">{props.description}</h3>
+    <div className="flex ">
+      <div>
+        <div className="flex ">
+          <button>
+            <img src={isCompleted ? NotDone : done} height={4} width={16}></img>
+          </button>
+          <h1 className="text-sm ml-6">{props.title}</h1>
+        </div>
+
+        <h3 className="text-xs ml-10">{props.description}</h3>
+      </div>
+
+      <div>
+        <div className="ml-56">
+          <div className="flex ">
+            <h1 className="text-sm ">{props.owner}</h1>
+          </div>
+
+          <h3 className="text-xs ">{props.time}</h3>
+        </div>
+      </div>
     </div>
   );
 }
