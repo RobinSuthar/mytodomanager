8f26d5963235ba7284ff6af7a96e7f34bf4ea41e
RobinSuthar <152766032+RobinSuthar@users.noreply.github.com>
2025-01-16 20:35:57 -0700

    Update Selection.jsx

--- src/components/NavigationBar/Selection.jsx
+++ src/components/NavigationBar/Selection.jsx
@@ -12,7 +12,7 @@ export function Selection() {
         </Link>
       </div>
       <div className="flex flex-row justify-left md:justify-center mt-1 mr-12">
-        <Link to="/Personal">
+        <Link to="/personal">
           <div className="text-base  md:text-lg hover:underline hover:text-xl hover:border-2  p-1 md:p-3 hover:border-green-500 rounded-2xl  ml-2 mt-4 md:mt-1">
             {" "}
             Personal{" "}
@@ -21,7 +21,7 @@ export function Selection() {
       </div>
 
       <div className="flex text-left flex-row justify-left md:justify-center mt-3 mr-12">
-        <Link to="/Organization" className="">
+        <Link to="/organization" className="">
           <div className=" text-base md:text-lg hover:underline hover:text-xl hover:border-2  p-1 md:p-3 hover:border-green-500 rounded-2xl mt-2    ml-2 md:mt-1">
             {" "}
             Company{" "}
