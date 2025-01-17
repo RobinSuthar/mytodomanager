2a26d0b53eb32fd3ebd652409c389cc1af95bbc7
RobinSuthar <152766032+RobinSuthar@users.noreply.github.com>
2025-01-17 02:39:25 -0700

    back

--- src/components/NavigationBar/Selection.jsx
+++ src/components/NavigationBar/Selection.jsx
@@ -2,30 +2,34 @@ import { Link } from "react-router-dom";
 
 export function Selection() {
   return (
-    <div className="font-Bungee flex flex-col md:flex-row md:gap-8 text-sm md:text-lg text-left p-5">
-      <div className="flex flex-col items-center md:items-start">
-        <Link to="/">
-          <div className="text-base md:text-lg hover:underline hover:text-xl hover:border-2 p-2 md:p-4 hover:border-blue-500 rounded-lg bg-white shadow-md hover:shadow-lg transition-all">
-            Global
-          </div>
-        </Link>
+    
+      <div className="font-Bungee flex md:flex-col  text-sm md:text-xl text-left">
+        <div className="  flex flex-row  justify-left md:justify-center  mt-4 mr-14">
+          <Link to="/">
+            <div className="text-base md:text-lg hover:underline   hover:text-xl hover:border-2  p-1 md:p-3 hover:border-green-500 rounded-2xl mt-1   ml-2 md:mt-2">
+              {" "}
+              Global{" "}
+            </div>
+          </Link>
+        </div>
+        <div className="flex flex-row justify-left md:justify-center mt-1 mr-12">
+          <Link to="/personal">
+            <div className="text-base  md:text-lg hover:underline hover:text-xl hover:border-2  p-1 md:p-3 hover:border-green-500 rounded-2xl  ml-2 mt-4 md:mt-1">
+              {" "}
+              Personal{" "}
+            </div>
+          </Link>
+        </div>
+
+        <div className="flex text-left flex-row justify-left md:justify-center mt-3 mr-12">
+          <Link to="/organization" className="">
+            <div className=" text-base md:text-lg hover:underline hover:text-xl hover:border-2  p-1 md:p-3 hover:border-green-500 rounded-2xl mt-2    ml-2 md:mt-1">
+              {" "}
+              Company{" "}
+            </div>
+          </Link>
+        </div>
       </div>
 
-      <div className="flex flex-col items-center md:items-start mt-4 md:mt-0">
-        <Link to="/personal">
-          <div className="text-base md:text-lg hover:underline hover:text-xl hover:border-2 p-2 md:p-4 hover:border-blue-500 rounded-lg bg-white shadow-md hover:shadow-lg transition-all">
-            Personal
-          </div>
-        </Link>
-      </div>
-
-      <div className="flex flex-col items-center md:items-start mt-4 md:mt-0">
-        <Link to="/organization">
-          <div className="text-base md:text-lg hover:underline hover:text-xl hover:border-2 p-2 md:p-4 hover:border-blue-500 rounded-lg bg-white shadow-md hover:shadow-lg transition-all">
-            Company
-          </div>
-        </Link>
-      </div>
-    </div>
   );
 }
