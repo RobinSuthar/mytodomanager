51e6fdbff943097459bd9e8a73d80e63a9383ef5
RobinSuthar <152766032+RobinSuthar@users.noreply.github.com>
2025-01-17 00:53:30 -0700

    Added new way of routing

--- src/components/NavigationBar/Selection.jsx
+++ src/components/NavigationBar/Selection.jsx
@@ -1,33 +1,44 @@
 import { Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "../Homepage.jsx";
+import Organization from "../Organization.jsx";
+import Indiviual from "../IndiviualTodo.jsx";
 
 export function Selection() {
   return (
-    <div className="font-Bungee flex md:flex-col  text-sm md:text-xl text-left">
-      <div className="  flex flex-row  justify-left md:justify-center  mt-4 mr-14">
-        <Link to="/">
-          <div className="text-base md:text-lg hover:underline   hover:text-xl hover:border-2  p-1 md:p-3 hover:border-green-500 rounded-2xl mt-1   ml-2 md:mt-2">
-            {" "}
-            Global{" "}
-          </div>
-        </Link>
-      </div>
-      <div className="flex flex-row justify-left md:justify-center mt-1 mr-12">
-        <Link to="/personal">
-          <div className="text-base  md:text-lg hover:underline hover:text-xl hover:border-2  p-1 md:p-3 hover:border-green-500 rounded-2xl  ml-2 mt-4 md:mt-1">
-            {" "}
-            Personal{" "}
-          </div>
-        </Link>
-      </div>
+    <BrowserRouter>
+      <div className="font-Bungee flex md:flex-col  text-sm md:text-xl text-left">
+        <div className="  flex flex-row  justify-left md:justify-center  mt-4 mr-14">
+          <Link to="/">
+            <div className="text-base md:text-lg hover:underline   hover:text-xl hover:border-2  p-1 md:p-3 hover:border-green-500 rounded-2xl mt-1   ml-2 md:mt-2">
+              {" "}
+              Global{" "}
+            </div>
+          </Link>
+        </div>
+        <div className="flex flex-row justify-left md:justify-center mt-1 mr-12">
+          <Link to="/personal">
+            <div className="text-base  md:text-lg hover:underline hover:text-xl hover:border-2  p-1 md:p-3 hover:border-green-500 rounded-2xl  ml-2 mt-4 md:mt-1">
+              {" "}
+              Personal{" "}
+            </div>
+          </Link>
+        </div>
 
-      <div className="flex text-left flex-row justify-left md:justify-center mt-3 mr-12">
-        <Link to="/organization" className="">
-          <div className=" text-base md:text-lg hover:underline hover:text-xl hover:border-2  p-1 md:p-3 hover:border-green-500 rounded-2xl mt-2    ml-2 md:mt-1">
-            {" "}
-            Company{" "}
-          </div>
-        </Link>
+        <div className="flex text-left flex-row justify-left md:justify-center mt-3 mr-12">
+          <Link to="/organization" className="">
+            <div className=" text-base md:text-lg hover:underline hover:text-xl hover:border-2  p-1 md:p-3 hover:border-green-500 rounded-2xl mt-2    ml-2 md:mt-1">
+              {" "}
+              Company{" "}
+            </div>
+          </Link>
+        </div>
       </div>
-    </div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/personal" element={<Indiviual />} />
+        <Route path="/organization" element={<Organization />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
